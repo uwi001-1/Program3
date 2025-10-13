0b1da16720c366b9457242f894ae6d406bc2a8d4
Kripa Hayanju <uwi001@Kripas-MacBook-Air.local>
2025-10-12 22:51:33 -0500

    them

---
+++ course.cpp
@@ -0,0 +1,25 @@
+#include "program3.h"
+
+//Default Constructor
+Course::Course()
+{
+    szCourseID = "XXX0000";
+    szCourseName = "Test Course";
+    iMaxStudents = 10;
+
+    studentEnrolled = new Student[iMaxStudents];
+    iNumEnrolled = 0;
+}
+
+//Parameterized Constructor
+Course::Course(string id, string name, int capacity)
+{
+    szCourseID = id;
+    szCourseName = name;
+    iMaxStudents = capacity;
+
+    studentEnrolled = new Student[iMaxStudents];
+    iNumEnrolled = 0;
+}
+
+//Copy constructor
